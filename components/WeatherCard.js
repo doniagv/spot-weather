5bcceca9c8e1c4593e3959b9a34e65ab294c2bac
Javier Gonzalez <doniagv@gmail.com>
2024-05-12 15:40:44 -0600

    Added more conditions and different card styles

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -7,8 +7,32 @@ export default function WeatherCard(props) {
   let dataDate = date.parse(props.data.location.localtime, "YYYY-MM-DD H:mm");
   let currentDate = date.format(dataDate, "ddd, MMM DD YYYY");
 
+  const getCardStyle = (conditionText) => {
+    console.log("🚀 ~ getCardStyle ~ conditionText:", conditionText);
+    conditionText = conditionText.toLowerCase(); // Make it case-insensitive
+    if (conditionText.includes("sunny")) {
+      return styles.CardSunny;
+    } else if (conditionText.includes("cloudy")) {
+      return styles.CardPartlyCloudy;
+    } else if (conditionText.includes("rain") || conditionText.includes("drizzle")) {
+      return styles.CardRain;
+    }
+    // else if (conditionText.includes("snow")) {
+    //   return styles.CardSnowy;
+    // } else if (conditionText.includes("thunder") || conditionText.includes("storm")) {
+    //   return styles.CardStormy;
+    // } else if (conditionText.includes("fog") || conditionText.includes("mist")) {
+    //   return styles.CardFoggy;
+    // } else if (conditionText.includes("wind")) {
+    //   return styles.CardWindy;
+    // }
+    else {
+      return styles.Card; // Default style
+    }
+  };
+
   return (
-    <div className={conditionText.includes("rain") ? styles.CardCloudy : styles.Card}>
+    <div className={getCardStyle(conditionText)}>
       <div className={styles.TemperatureSection}>
         <p className={styles.ConditionText}>{props.data.current.condition.text}</p>
         <p className={styles.TempC}>{props.data.current.temp_c}°</p>
